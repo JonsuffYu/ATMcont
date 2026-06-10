0619af360b038495203d4138667c328c9bede737
JonsuffYu <dbwhdtjq92@gmail.com>
2026-06-11 01:09:53 +0900

    Enhanced directory policy

--- include/json_handler.hpp
+++ include/json_handler.hpp
@@ -7,10 +7,11 @@
 #include <sstream>
 #include <string>
 
+inline std::string file_name = "card_account_db.json";
 
 inline nlohmann::json load_json_db() {
     try {
-        std::ifstream file("../local_db/card_account_db.json");
+        std::ifstream file(file_name);
         if (!file) {
             std::cerr << "Cannot find json file for DB" << std::endl;
             return nlohmann::json();
@@ -19,11 +20,20 @@ inline nlohmann::json load_json_db() {
         std::stringstream buffer;
         buffer << file.rdbuf();
 
-        // nlohmann::json loaded_data_ = nlohmann::json::parse(buffer.str());
         nlohmann::json loaded_data = nlohmann::json::parse(buffer.str());
         return loaded_data;
     } catch (const std::exception& e) {
         std::cerr << "JSON error: " << e.what() << std::endl;
         return nlohmann::json();
     }
+}
+
+inline bool dump_json(const nlohmann::json& json_data) {
+    std::ofstream file(file_name);
+    if (!file.is_open()) {
+        std::cerr << "Cannot open file" << std::endl;
+        return false;
+    }
+    file << json_data.dump(4);
+    return true;
 }
