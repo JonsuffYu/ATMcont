5870da14939cb286874ec88829d7f398ba91311a
JonsuffYu <dbwhdtjq92@gmail.com>
2026-06-10 21:16:35 +0900

    Added main function to launch atm controller

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ enum class ATMPhase
 {
     INSERT_CARD,
     VERIFY_PIN,
+    SELECT_ACCOUNT,
     SELECT_MENU,
     DEPOSIT,
     WITHDRAW,
@@ -18,10 +19,10 @@ std::shared_ptr<ATMController> atm_controller;
 
 int main() {
     ATMPhase current_phase = ATMPhase::INSERT_CARD;
-    atm_controller = std::make_shared<ATMController>();
     while (current_phase != ATMPhase::EXIT) {
         switch (current_phase) {
             case ATMPhase::INSERT_CARD: {
+                atm_controller = std::make_shared<ATMController>();
                 std::cout << "Enter card or type card number without '-'" << std::endl;
                 std::string card_info, pin_info;
                 std::cin >> card_info;
@@ -44,8 +45,51 @@ int main() {
                     current_phase = ATMPhase::INSERT_CARD;
                     break;
                 }
-                current_phase = ATMPhase::SELECT_MENU;
+                current_phase = ATMPhase::SELECT_ACCOUNT;
                 break;
+            }
+            case ATMPhase::SELECT_ACCOUNT: {
+                std::vector<std::string> account_candidates = atm_controller->get_account_num();
+                std::string sample;
+                int i = 0;
+                for (auto & account_num : account_candidates) {
+                    i += 1;
+                    sample += std::to_string(i) + ") " + account_num + ", ";
+                }
+                std::cout << "Select account:" << sample << std::endl;
+                std::string account_number;
+                std::cin >> account_number;
+            }
+            case ATMPhase::SELECT_MENU: {
+                std::string name = atm_controller->get_account_name();
+                std::string print_out;
+                print_out += "Hello " + name;
+                print_out += ", Select menu: 1) DEPOSIT, 2) WITHDRAW, 3) TRANSFER, 4) BALANCE_INQUIRY, 5) EXIT";
+                std::cout << print_out << std::endl;
+                std::string menu;
+                std::cin >> menu;
+                if (menu == "1") {
+                    current_phase = ATMPhase::DEPOSIT;
+                } else if (menu == "2") {
+                    current_phase = ATMPhase::WITHDRAW;
+                } else if (menu == "3") {
+                    current_phase = ATMPhase::TRANSFER;
+                } else if (menu == "4") {
+                    current_phase = ATMPhase::BALANCE_INQUIRY;
+                } else if (menu == "5") {
+                    std::cout << "Exit selected. Return to card inserting menu" << std::endl;
+                    current_phase = ATMPhase::INSERT_CARD;
+                } else {
+                    std::cerr << "Invalid menu clicked, try again" << std::endl;
+                    current_phase = ATMPhase::SELECT_MENU;
+                    break;
+                }
+            }
+            case ATMPhase::DEPOSIT: {
+                std::cout << "Input cash and enter the amount of money" << std::endl;
+                std::string amount_money;
+                std::cin >> amount_money;
+                // static_cast
             }
         }
     }
