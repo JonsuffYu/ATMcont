d475b697acea63737549aa51b9617e74dc63feed
JonsuffYu <dbwhdtjq92@gmail.com>
2026-06-11 01:08:05 +0900

    Enhanced account handler

--- include/account_handler.hpp
+++ include/account_handler.hpp
@@ -12,13 +12,19 @@ class AccountHandler {
 public:
     explicit AccountHandler();
     virtual ~AccountHandler();
-    bool check_if_card_info_is_valid(const std::string & card_info);
+
+    bool check_if_card_info_is_valid(std::string & card_info);
     bool verify_pin(const std::string & card_info, const std::string & pin_number);
-    std::string get_account_name(const std::string & card_info);
-    std::vector<std::string> get_account_num(const std::string & card_info);
+    std::string get_account_owner(const std::string & card_info);
+    std::vector<std::string> get_account_nums(const std::string & card_info);
+    std::string get_owner_name_to_transfer(const std::string & account, const std::string & bank_name);
+    void set_account(const std::string & selected_account);
+    void reset_selected_account();
+    std::string get_selected_account();
+    void reset_account_selection();
 
 private:
     inline static nlohmann::json loaded_data_ = load_json_db();
-
+    std::string selected_account_;
     void init_account_handler();
 };
--- src/account_handler.cpp
+++ src/account_handler.cpp
@@ -6,7 +6,7 @@ AccountHandler::AccountHandler()
 AccountHandler::~AccountHandler()
 {}
 
-bool AccountHandler::check_if_card_info_is_valid(const std::string & card_info) {
+bool AccountHandler::check_if_card_info_is_valid(std::string & card_info) {
     if (!loaded_data_.contains(card_info)) {
         std::cerr << "Failed to find certain card info: " << card_info << std::endl;
         return false;
@@ -22,14 +22,41 @@ bool AccountHandler::verify_pin(const std::string & card_info, const std::string
     return true;
 }
 
-std::string AccountHandler::get_account_name(const std::string & card_info) {
+std::string AccountHandler::get_account_owner(const std::string & card_info) {
     return loaded_data_[card_info]["name"].get<std::string>();
 }
 
-std::vector<std::string> AccountHandler::get_account_num(const std::string & card_info) {
+std::vector<std::string> AccountHandler::get_account_nums(const std::string & card_info) {
     std::vector<std::string> accounts_per_name;
     for (auto & [account_num, info] : loaded_data_[card_info]["accounts"].items()) {
         accounts_per_name.push_back(account_num);
     }
     return accounts_per_name;
+}
+
+std::string AccountHandler::get_owner_name_to_transfer(
+    const std::string & account,
+    const std::string & bank_name)
+{
+    std::string name;
+    for (auto & [card_num, item] : loaded_data_.items()) {
+        auto & accounts = item["accounts"];
+        if (accounts.contains(account) && accounts[account]["bank_name"] == bank_name) {
+            name = item["name"];
+            break;
+        }
+    }
+    return name;
+}
+
+void AccountHandler::set_account(const std::string & selected_account) {
+    selected_account_ = selected_account;
+}
+
+std::string AccountHandler::get_selected_account() {
+    return selected_account_;
+}
+
+void AccountHandler::reset_selected_account() {
+    selected_account_ = "";
 }
